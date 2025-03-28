634371219f13aa24fd90dccc4387c3d8b27ad278
kacperwolanski <kacpertomasz00@gmail.com>
2025-03-28 10:33:38 +0100

    adjust phone number display

--- turbohydraulik/app/components/shared/helpers/convertPhoneNum.ts
+++ turbohydraulik/app/components/shared/helpers/convertPhoneNum.ts
@@ -1,5 +1,13 @@
 import { defaultPhoneNUmber } from "@/app/constants/appConstants";
 
 export const convertPhoneNum = (phoneNumber?: string) => {
-  return `+48 ${phoneNumber ? phoneNumber : defaultPhoneNUmber}`;
+  const sanitizedNumber = (phoneNumber || defaultPhoneNUmber).replace(
+    /\s+/g,
+    ""
+  );
+  const formattedNumber = sanitizedNumber.replace(
+    /(\d{3})(\d{3})(\d{3})/,
+    "$1 $2 $3"
+  );
+  return `+48 ${formattedNumber}`;
 };
