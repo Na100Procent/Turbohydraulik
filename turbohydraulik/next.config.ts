527907345b0b4bc7cbad2dfcb072a1cc86f9a357
kacperwolanski <kacpertomasz00@gmail.com>
2025-04-06 18:57:41 +0200

    additional security headers

--- turbohydraulik/next.config.ts
+++ turbohydraulik/next.config.ts
@@ -42,6 +42,14 @@ const nextConfig = {
               "worker-src 'self';",
           },
           {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
