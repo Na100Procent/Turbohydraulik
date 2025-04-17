e8b74f842234e885eff4a1ba2cee74bc2b020e6f
Kamil <k.muras88@gmail.com>
2025-04-17 14:07:44 +0200

    zmiana url opini wroclaw

--- turbohydraulik/app/data/cityReviewLinks.ts
+++ turbohydraulik/app/data/cityReviewLinks.ts
@@ -1,5 +1,5 @@
 export const cityReviewLinks: { [key: string]: string } = {
-  wroclaw: "https://g.page/r/CUq3ws85F_8REBM/review/wroclaw",
+  wroclaw: "https://g.page/r/CYBl-O8-QXWzEBM/review",
   gdynia: "https://g.page/r/ABCDEF123456/review/gdynia",
   bydgoszcz: "https://g.page/r/GHIJKL654321/review/bydgoszcz",
   krakow: "https://g.page/r/MNOPQR789012/review/krakow",
